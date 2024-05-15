0f174b339dba2fe3befb91b5fb1e44e5d2514fab
zabihhaqqani <mdzabihhaqqani@gmail.com>
2024-05-15 12:55:49 +0530

    styling: fix admin panel table

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -49,42 +49,44 @@ export default function Admin() {
   return (
     <div className="md:text-lg lg:text-lg text-sm">
       <Navbar />
-      <div className="flex flex-col items-center justify-between p-10 w-[100%]">
+      <div className="flex flex-col items-center justify-between p-5 ">
         <h1 className="font-semibold text-2xl my-3">Admin Panel</h1>
         <h3 className="">Form Submissions</h3>
-        {formData.length > 0 ? (
-          <Table className="w-[90%]">
-            <TableHeader>
-              <TableRow>
-                <TableHead>Name</TableHead>
-                <TableHead>Email</TableHead>
-                <TableHead>Message</TableHead>
-                <TableHead></TableHead>
-                <TableHead></TableHead>
-              </TableRow>{" "}
-            </TableHeader>
-            <TableBody>
-              {formData?.map((data) => (
-                <TableRow key={data.id}>
-                  <TableCell>{data.name}</TableCell>
-                  <TableCell>{data.email}</TableCell>
-                  <TableCell>{data.message}</TableCell>
-                  <Button
-                    type="submit"
-                    variant="default"
-                    onClick={() => deleteFormData(data.id)}
-                  >
-                    Delete
-                  </Button>
+        <div className="flex items-center justify-center w-[100%] md:w-[750px] sm:w-[600px]">
+          {formData.length > 0 ? (
+            <Table className="w-[90%]">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Name</TableHead>
+                  <TableHead>Email</TableHead>
+                  <TableHead>Message</TableHead>
+                  <TableHead></TableHead>
+                  <TableHead></TableHead>
+                </TableRow>{" "}
+              </TableHeader>
+              <TableBody>
+                {formData?.map((data) => (
+                  <TableRow key={data.id}>
+                    <TableCell>{data.name}</TableCell>
+                    <TableCell>{data.email}</TableCell>
+                    <TableCell>{data.message}</TableCell>
+                    <Button
+                      type="submit"
+                      variant="default"
+                      onClick={() => deleteFormData(data.id)}
+                    >
+                      Delete
+                    </Button>
 
-                  <Edit data={data} fetchData={() => fetchData()} />
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        ) : (
-          <p className="my-3">No Submissions!</p>
-        )}
+                    <Edit data={data} fetchData={() => fetchData()} />
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <p className="my-3">No Submissions!</p>
+          )}
+        </div>
       </div>
     </div>
   )
